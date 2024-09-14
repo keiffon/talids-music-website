4a29f51c9cc1f91ee85267419c976181921dd209
Keiffon A Broxton <keiffonbroxtonn@gmail.com>
2024-09-14 14:09:33 -0400

    .1

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-const musicList = [
-    {
-        title: "Track 1",
-        file: "music/track1.mp3"
-    },
-    {
-        title: "Track 2",
-        file: "music/track2.mp3"
-    }
-    // Add more tracks here
-];
+var mySong = document.getElementById("mySong");
+var icon = document.getElementById("icon");
 
-const ul = document.getElementById('musicList');
-const audioPlayer = document.getElementById('audioPlayer');
-const nowPlaying = document.getElementById('nowPlaying');
-
-musicList.forEach((track, index) => {
-    const li = document.createElement('li');
-    li.textContent = track.title;
-    li.addEventListener('click', () => {
-        audioPlayer.src = track.file;
-        audioPlayer.play();
-        nowPlaying.textContent = `Now Playing: ${track.title}`;
-    });
-    ul.appendChild(li);
-});
+    icon.onclick = function(){
+        if(mySong.paused){
+            mySong.play();
+            icon.src = "media/pause.png";
+            }else{
+            mySong.pause();
+            icon.src = "media/play.png";
+            }
+        };
