a0cd0569b1b688bba42c55ecf8f1f805bfc02135
Keiffon A Broxton <keiffonbroxtonn@gmail.com>
2024-11-13 23:09:48 -0500

    halfway point
    redesigned the website to clients liking

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-var mySong = document.getElementById("mySong");
-var icon = document.getElementById("icon");
+const menuBtn= document.querySelector(".menu-btn");
+const navigation= document.querySelector(".navigation");
 
-    icon.onclick = function(){
-        if(mySong.paused){
-            mySong.play();
-            icon.src = "media/pause.png";
-            }else{
-            mySong.pause();
-            icon.src = "media/play.png";
-            }
-        };
-
-       function showSidebar() {
-        const sidebar = document.querySelector('.sidebar')
-        sidebar.style.display = 'flex'
-       };
-
-       function hideSidebar() {
-        const sidebar = document.querySelector('.sidebar')
-        sidebar.style.display = 'none'
-       }
-        
+menuBtn.addEventListener("click", ()=> {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
 
     
